3f3ae4e3a59ef58880b51228bb3b0cc4c5210ca9
Hai Hoang <haicanberra@gmail.com>
2023-09-21 01:04:40 -0600

    Debugging evaluate

--- debug.py
+++ debug.py
@@ -5,5 +5,5 @@ temp = [184613, 318219, 391895, 522418]
 with open("dataset\\nobug\\val\\annotations\\captions_val2014.json", "r") as file:
     data = json.load(file)
     for key in data["annotations"]:
-        if key["image_id"] in temp:
-            print(key)
+        if len(key['caption']) == 250:
+            print(key['caption'])
--- evaluate.py
+++ evaluate.py
@@ -6,43 +6,56 @@ from config import *
 from PIL import Image
 
 # Path to the checkpoint file
-checkpoint_path = "checkpoints/model_checkpoint_epoch20.pth"
+# MODIFY THIS THEN CHECK LINE 32
+checkpoint_path = "image_captioning_model.pth"
 
 # Initialize data preprocessing and tokenizer
+# Comment if already saved vocab file outside.
 mscoco = MSCOCO()
 
-# Image preprocessing transform
-image_transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Resize((299, 299), antialias=True),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    ]
-)
+# Save the vocab, comment all lines after 18.
+torch.save(mscoco.train_vocab, 'train_vocab.pth')
 
-# Load the model from the checkpoint
-model = CNNLSTM(
-    embed_size=EMBED_SIZE,
-    hidden_size=HIDDEN_SIZE,
-    vocab_size=len(mscoco.train_vocab),
-    num_layers=NUM_LAYERS,
-)
-checkpoint = torch.load(checkpoint_path)
-model.load_state_dict(checkpoint["model_state_dict"])
-model.eval()
+# # Load the vocab
+# train_vocab = torch.load('train_vocab.pth')
 
-# Load an image for testing
-image_path = "evaluate\\test.jpg"
-test_image = Image.open(image_path).convert("RGB")
-test_image = image_transform(test_image).unsqueeze(0)
-print(test_image.shape)
+# # Image preprocessing transform
+# image_transform = transforms.Compose(
+#     [
+#         transforms.ToTensor(),
+#         transforms.Resize((299, 299), antialias=True),
+#         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+#     ]
+# )
 
-# Generate a caption for the test image
-with torch.no_grad():
-    caption_indices = model.caption(
-        test_image, mscoco.train_vocab, max_caption_length=MAX_CAPTION_LENGTH
-    )
-    generated_caption = mscoco.decode_caption(caption_indices)
+# # Load the model from the checkpoint
+# model = CNNLSTM(
+#     embed_size=EMBED_SIZE,
+#     hidden_size=HIDDEN_SIZE,
+#     vocab_size=len(train_vocab),
+#     num_layers=NUM_LAYERS,
+# )
+# checkpoint = torch.load(checkpoint_path)
+# model.load_state_dict(checkpoint["state_dict"])
+# model.eval()
 
-# Print the generated caption
-print("Generated Caption:", generated_caption)
+# # Load an image for testing
+# image_path = "evaluate\\test.jpg"
+# test_image = Image.open(image_path).convert("RGB")
+# test_image = image_transform(test_image).unsqueeze(0)
+# print(test_image.shape)
+
+# def decode_caption(indices):
+#     # Convert index to string, join and remove paddings.
+#     words = [train_vocab.lookup_token(index) for index in indices]
+#     return " ".join(words).replace("<PAD>", "")
+
+# # Generate a caption for the test image
+# with torch.no_grad():
+#     caption_indices = model.caption(
+#         test_image, train_vocab, max_caption_length=MAX_CAPTION_LENGTH
+#     )
+#     generated_caption = decode_caption(caption_indices)
+
+# # Print the generated caption
+# print("Generated Caption:", generated_caption)
